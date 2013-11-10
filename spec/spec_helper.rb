ffaf91fa5729f070e757a2fa5c588fbd86d8f94c
Max Lincoln <max@devopsy.com>
2013-11-10 20:03:06 -0300

    Support passing vars to tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require 'pacto'
+require 'tempfile'
 
 SDKs = Dir['sdks/*'].map{|sdk| File.basename sdk}
 
@@ -6,7 +7,7 @@ RSpec.configure do |c|
   c.treat_symbols_as_metadata_keys_with_true_values = true
 end
 
-def invoke_challenge sdk, challenge
+def invoke_challenge sdk, challenge, vars = standard_env_vars
   sdk_dir = "sdks/#{sdk}"
   pending "#{sdk} is not setup" unless File.directory? sdk_dir
   Bundler.with_clean_env do
@@ -14,12 +15,31 @@ def invoke_challenge sdk, challenge
       challenge_script = Dir["challenges/#{challenge}.*"].first
       pending "Challenge #{challenge} is not implemented" if challenge_script.nil?
       # Do bootstrap ahead of challenge?
-      `scripts/bootstrap`
+      # `scripts/bootstrap`
+      env_file = setup_env_vars vars
       if File.exists? "scripts/wrapper"
-        system "scripts/wrapper #{challenge_script}"
+        system "source #{env_file} && scripts/wrapper #{challenge_script}"
       else
-        system "./#{challenge_script}"
+        system "source #{env_file} && ./#{challenge_script}"
       end
     end
   end
+end
+
+def setup_env_vars vars
+  file = Tempfile.new('vars')
+  vars.each do |key, value|
+    file.write("export #{key}=#{value}\n")
+  end
+  file.close
+  file.path
+end
+
+def standard_env_vars
+  @standard_env_vars ||= {
+    'RAX_USERNAME'   => ENV['RAX_USERNAME'],
+    'RAX_API_KEY'    => ENV['RAX_API_KEY'],
+    'RAX_REGION'     => 'dfw',
+    'RAX_AUTH_URL'   => ENV['PACTO_SERVER'] || 'https://identity.api.rackspacecloud.com'
+  }
 end
