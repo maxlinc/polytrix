6725555288bc552d94eba060563d07c0d8b61c38
Max Lincoln <max@devopsy.com>
2013-11-11 13:01:19 -0300

    Use custom auth endpoint (hook into Pacto) for pkgcloud

--- sdks/pkgcloud/challenges/authenticate.js
+++ sdks/pkgcloud/challenges/authenticate.js
@@ -3,7 +3,8 @@ var pkgcloud = require('pkgcloud');
 // create our client with your rackspace credentials
 var client = pkgcloud.providers.rackspace.compute.createClient({
   username: process.env.RAX_USERNAME,
-  apiKey: process.env.RAX_API_KEY
+  apiKey: process.env.RAX_API_KEY,
+  authUrl: process.env.RAX_AUTH_URL
 });
 client.getImages(function(images) {
     console.log('Authenticated');
