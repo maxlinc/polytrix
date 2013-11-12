18e346ca377952a554bf8a38046ec0e113bd1c0c
Max Lincoln <max@devopsy.com>
2013-11-11 22:24:06 -0300

    add server challenge

---
+++ spec/servers_spec.rb
@@ -0,0 +1,16 @@
+describe 'managing servers' do
+  SDKs.each do |sdk|
+    context sdk, sdk.to_sym do
+      it 'create 2 servers' do
+        vars = standard_env_vars
+        vars.merge!({
+          'RAX_REGION' => 'DFW',
+          'SERVER1_IMAGE' => 'f70ed7c7-b42e-4d77-83d8-40fa29825b85',
+          'SERVER1_FLAVOR' => '2'
+        })
+        success = invoke_challenge sdk, "servers", vars
+        expect(success).to be_true
+      end
+    end
+  end
+end
