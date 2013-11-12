f8d09b780ed7174bc516102f47fc48845a7a6c2f
Max Lincoln <max@devopsy.com>
2013-11-11 22:24:33 -0300

    include gophercloud (though it doesn't do token auth)

---
+++ sdks/gophercloud/challenges/authenticate.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    // "github.com/rackspace/gophercloud"
+    // "os"
+)
+
+func main() {
+    // var provider = "rackspace-us"
+    // var username = os.Getenv("RAX_USERNAME")
+    // var api_key = os.Getenv("RAX_API_KEY")
+    // _, err := gophercloud.Authenticate(
+    //     provider,
+    //     gophercloud.AuthOptions{
+    //         Username: username,
+    //         Password: password,
+    //     },
+    // )
+    // if err != nil {
+    //     panic(err)
+    // }
+    // println("Authenticated")
+    println("Fail - gophercloud does not support authentication with an API_KEY")
+}
