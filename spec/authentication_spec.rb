6c5ffd47a82016df6045d1abd1db1701424f8265
Max Lincoln <max@devopsy.com>
2013-11-18 18:53:50 -0300

    Better assertions

--- spec/authentication_spec.rb
+++ spec/authentication_spec.rb
@@ -5,7 +5,9 @@ describe 'authentication' do
         validate_challenge sdk, "authenticate" do |success|
           # Assertions
           expect(success).to be_true
-          expect(WebMock).to have_requested(:post, 'https://identity.api.rackspacecloud.com/v2.0/tokens')
+          expect(Pacto).to have_validated(:post, 'https://identity.api.rackspacecloud.com/v2.0/tokens')
+          expect(Pacto).to_not have_failed_validations
+          expect(Pacto).to_not have_unmatched_requests
         end
       end
     end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,34 @@
 require 'pacto'
+require 'pacto/rspec'
 $:.unshift File.expand_path('../pacto', File.dirname(__FILE__))
 require 'webmock/rspec'
 require 'pacto_server'
 require 'tempfile'
 require 'goliath/test_helper'
 
+PACTO_SERVER = 'http://identity.api.rackspacecloud.dev:9900' unless ENV['NO_PACTO']
+
 SDKs = Dir['sdks/*'].map{|sdk| File.basename sdk}
 
 RSpec.configure do |c|
   c.treat_symbols_as_metadata_keys_with_true_values = true
   c.include Goliath::TestHelper
+
+  c.before(:each)  { Pacto.clear! }
 end
 
 def validate_challenge sdk, challenge, vars = standard_env_vars
   sdk_dir = "sdks/#{sdk}"
   pending "#{sdk} is not setup" unless File.directory? sdk_dir
-  with_api(PactoServer, {:log_file => 'pacto.log', :config => 'pacto/config/pacto_server.rb', :live => true, :verbose => true}) do
+  with_api(PactoServer, {
+    :log_file => 'pacto.log',
+    :config => 'pacto/config/pacto_server.rb',
+    :live => true,
+    :generate => true,
+    :verbose => true,
+    :validate => true,
+    :directory => File.join(Dir.pwd, 'pacto', 'contracts')
+    }) do
     EM::Synchrony.defer do
       Bundler.with_clean_env do
         Dir.chdir sdk_dir do
@@ -29,8 +42,9 @@ def validate_challenge sdk, challenge, vars = standard_env_vars
           else
             command = "source #{env_file} && ./#{challenge_script}"
           end
-          status = system command
-          yield status
+          success = system command
+          expect(success).to be_true, "#{sdk} failed to successfully execute the #{challenge} challenge"
+          yield success
         end
       end
       EM.stop
@@ -52,6 +66,6 @@ def standard_env_vars
     'RAX_USERNAME'   => ENV['RAX_USERNAME'],
     'RAX_API_KEY'    => ENV['RAX_API_KEY'],
     'RAX_REGION'     => 'DFW',
-    'RAX_AUTH_URL'   => ENV['PACTO_SERVER'] || 'https://identity.api.rackspacecloud.com'
+    'RAX_AUTH_URL'   => PACTO_SERVER || 'https://identity.api.rackspacecloud.com'
   }
 end
