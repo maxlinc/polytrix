3115ba5304f710b11d6ef4457303fe6756808e04
Max Lincoln <max@devopsy.com>
2013-11-22 00:01:13 -0300

    cleaning up sample

--- sdks/php-opencloud/challenges/servers.php
+++ sdks/php-opencloud/challenges/servers.php
@@ -12,25 +12,12 @@ $rackspace = new Rackspace($endpoint, $credentials);
 
 $compute = $rackspace->computeService('cloudServersOpenStack', getenv('RAX_REGION'));
 
-// $imagelist = $compute->ImageList();
-// $imagelist->Sort('name');
-// while($i = $imagelist->Next()) {
-//   // save a CentOS image for later
-//   if (!isset($centos) && isset($i->metadata->os_distro) &&
-//      $i->metadata->os_distro == 'centos') {
-//     $centos = $i;
-//   }
-// }
-// $flavorlist = $compute->FlavorList();
-
 $server = $compute->Server();
 $server->name = 'MyNewServer';
 $flavor_id = intval(getenv('SERVER1_FLAVOR'));
 
 $server->image = $compute->Image(getenv('SERVER1_IMAGE'));
 $server->flavor = $compute->Flavor($flavor_id);
-// $server->image = $centos;
-// $server->flavor = $flavorlist->First();
 
 // create it
 print("Creating server...");
@@ -42,7 +29,7 @@ print("done\n");
 exit(0);
 
 function dot($server) {
-        printf("%s %3d%%\n", $server->status, $server->progress);
+  printf("%s %3d%%\n", $server->status, $server->progress);
 }
 
 ?>
