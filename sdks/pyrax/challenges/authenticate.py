7b9274a03065e90c97e46932db01277b1545af9c
Max Lincoln <max@devopsy.com>
2013-11-22 00:12:25 -0300

    pyrax sample cleanup

--- sdks/pyrax/challenges/authenticate.py
+++ sdks/pyrax/challenges/authenticate.py
@@ -4,12 +4,12 @@ import os
 import pyrax
 
 pyrax.set_setting("identity_type", "rackspace")
+# Create the identity object
+pyrax._create_identity()
+# Change its endpoint
+pyrax.identity.auth_endpoint = os.getenv('RAX_AUTH_URL') + '/v2.0/'
+pyrax.set_http_debug(True)
+# Authenticate
 pyrax.set_credentials(os.getenv('RAX_USERNAME'), os.getenv('RAX_API_KEY'))
 
-pyrax.set_http_debug(True)
-pyrax.identity.auth_endpoint = os.getenv('RAX_AUTH_URL') + '/v2.0/'
-# pyrax.identity.authenticate()
-
-cs = pyrax.cloudservers
-print cs.images.list()
 print "Authenticated"
