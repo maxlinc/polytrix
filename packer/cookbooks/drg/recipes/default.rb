029bcb89e3230b3dc37d46e755755a47c7649543
Max Lincoln <max@devopsy.com>
2014-01-03 16:18:36 -0500

    Use go 1.1.1

--- packer/cookbooks/drg/recipes/default.rb
+++ packer/cookbooks/drg/recipes/default.rb
@@ -1,7 +1,7 @@
 include_recipe 'drg::system'
 include_recipe 'python'
 include_recipe 'drg::ruby'
-include_recipe 'golang'
+include_recipe 'drg::golang'
 include_recipe "nodejs::install_from_package"
 include_recipe 'drg::php'
 include_recipe 'drg::java'
