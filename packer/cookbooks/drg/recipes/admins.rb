1b7850b050d7f23f0b390580f47f9175a3fb8667
Max Lincoln <max@devopsy.com>
2014-01-15 15:12:57 -0600

    No backup admin... should use keypairs on creation instead (if jenkins-jclouds supports it)

--- packer/cookbooks/drg/recipes/admins.rb
+++ packer/cookbooks/drg/recipes/admins.rb
@@ -8,38 +8,15 @@ user "jenkins" do
   home '/jenkins'
 end
 
-# Make the person who created the image a backup admin user
-backup_admin = ENV["RAX_USERNAME"]
-log "Creating user #{backup_admin} as a backup admin"
-
-user backup_admin do
-  supports :manage_home => true
-  home "/home/#{backup_admin}"
-end
-
 group "sudo" do
   action :modify
-  members ["jenkins", backup_admin]
+  members ["jenkins"]
   append true
 end
 
 # /etc/sudoers should be set for "sudo" instead of "wheel", but let's add them just in case
 group "wheel" do
   action :modify
-  members ["jenkins", backup_admin]
+  members ["jenkins"]
   append true
-end
-
-directory "/home/#{backup_admin}/.ssh" do
-  owner backup_admin
-  group backup_admin
-  mode 0700
-  action :create
-end
-
-file "/home/#{backup_admin}/.ssh/authorized_keys" do
-  owner backup_admin
-  group backup_admin
-  mode 0600
-  content File.read(File.expand_path("~/.ssh/id_rsa.pub"))
 end
