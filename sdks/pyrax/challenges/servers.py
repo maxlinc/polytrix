709afe2eaeceb02dac0fbaf93811bd43edde11c1
Max Lincoln <max@devopsy.com>
2014-01-20 14:22:42 -0600

    Update docs for servers.

--- sdks/pyrax/challenges/servers.py
+++ sdks/pyrax/challenges/servers.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python
 
+# Overcommented to illustrate docco....
+
 import os
 import pyrax
+# [Configure Pyrax](https://github.com/rackspace/pyrax/blob/master/docs/getting_started.md#pyrax-configuration)
+# to use the Rackspace identity service.
 
 pyrax.set_setting("identity_type", "rackspace")
-# Create the identity object
-pyrax._create_identity()
-# Change its endpoint
-pyrax.identity.auth_endpoint = os.getenv('RAX_AUTH_URL') + '/v2.0/'
-pyrax.set_setting('region', os.getenv('RAX_REGION'))
 
-# Authenticate
+
+# Change the identity endpoint (to enable test assertions w/ Pacto)
+pyrax._create_identity()
+pyrax.identity.auth_endpoint = os.getenv('RAX_AUTH_URL') + '/v2.0/'
+
+# [Authenticate](https://github.com/rackspace/pyrax/blob/master/docs/getting_started.md#authenticating)
+# using an API key.
 pyrax.set_credentials(os.getenv('RAX_USERNAME'), os.getenv('RAX_API_KEY'))
 
+# Set the region, flavor, and image for the test scenario.
+pyrax.set_setting('region', os.getenv('RAX_REGION'))
 flavor = os.getenv('SERVER1_FLAVOR')
 image  = os.getenv('SERVER1_IMAGE')
+
+# Create a [Cloud Servers](https://github.com/rackspace/pyrax/blob/master/docs/cloud_servers.md) connection.
 cs = pyrax.cloudservers
+
+# [Create a server](https://github.com/rackspace/pyrax/blob/master/docs/cloud_servers.md#creating-a-server)
 server = cs.servers.create("Pyrax Server", image, flavor)
+# and [wait for it to build](https://github.com/rackspace/pyrax/blob/master/docs/cloud_servers.md#waiting-for-server-completion).
 pyrax.utils.wait_for_build(server, verbose=True)
--- spec/servers_spec.rb
+++ spec/servers_spec.rb
@@ -1,11 +1,22 @@
-describe 'Servers' do
-  describe 'creates a server' do
-    vars = standard_env_vars
-    vars.merge!({
+CHALLENGE = <<-EOS
+[Create a Server](http://docs.rackspace.com/servers/api/v2/cs-devguide/content/CreateServers.html)
+using the image and flavor, and region specified in the environment.
+EOS
+
+ENVIRONMENT = standard_env_vars.merge({
       'RAX_REGION' => 'DFW',
       'SERVER1_IMAGE' => 'f70ed7c7-b42e-4d77-83d8-40fa29825b85',
       'SERVER1_FLAVOR' => '2'
     })
+
+# TBD... will load service contracts via Pacto, and automatically link to service documentation
+SERVICES = []
+
+describe 'Servers' do
+  describe 'Create Server' do
+    :markdown => CHALLENGE,
+    "data-environment" => redact(ENVIRONMENT),
+    "data-services" => SERVICES
     validate_challenge "servers", vars do |success|
       # Assertions
       expect(Pacto).to have_validated(:post, /dfw.servers.api.rackspacecloud.com\/v2\/\d+\/servers/) #.twice
