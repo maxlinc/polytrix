894ed9ca67f6aa556dfc61193f912e83833e3e6b
Max Lincoln <max@devopsy.com>
2014-03-04 16:15:20 -0500

    Extensions for loading simple service description until full contracts or api blueprints are available

---
+++ spec/pacto/extensions/loaders/simple_loader.rb
@@ -0,0 +1,50 @@
+require 'multi_json'
+require 'yaml'
+require 'pacto'
+require_relative 'yaml_or_json_loader'
+
+class Pacto::Extensions::Loaders::URIMapLoader < Pacto::Extensions::Loaders::YamlOrJsonLoader
+  def self.load(file)
+    data = super
+    contracts = []
+    data['services'].each do | group_name, service_group |
+      service_group['servers'].each do | server |
+        if service_group['services']
+          service_group['services'].each_pair do | service_name, service_definition|
+            contract = build_simple_contract service_definition, group_name, service_name, server, file
+            Pacto.contract_registry.register(contract)
+            contracts << contract
+          end
+        end
+      end
+    end
+    Pacto::ContractList.new contracts
+  end
+
+  private
+  def self.build_simple_contract service_definition, group_name, service_name, server, file
+    service_signature = "#{service_definition['method'].upcase} #{service_definition['uriTemplate']}"
+    puts "Registering '#{service_signature}' as '#{group_name} - #{service_name}' on #{server}"
+    # FIXME: What about scheme?
+    host = "https://#{server}:443"
+    request_clause = Pacto::RequestClause.new(host, {
+      'method' => service_definition['method'],
+      'headers' => [], #not supporting this yet, probably needs conversion
+      'path' => convert_template(service_definition['uriTemplate'])
+    })
+    response_clause = Pacto::ResponseClause.new({
+      'status' => service_definition['responseStatusCode'] || 200,
+      'headers' => [], #not supporting this yet, probably needs conversion
+      'body' => {}
+    })
+    Pacto::Contract.new(request_clause, response_clause, file, service_name)
+  end
+
+  def self.convert_template path
+    path.gsub(/{(\w+)}/, ':\1') if path
+  end
+end
+
+# Pacto.configuration.strict_matchers = false
+# contracts = Pacto::Extensions::Loaders::URIMapLoader.load('pacto/uri_map.yaml')
+# require 'pry'; binding.pry
