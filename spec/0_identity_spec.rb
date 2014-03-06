fdfc111d644806e02ed791db639e3f65a54e4b24
Max Lincoln <max@devopsy.com>
2014-03-06 15:13:43 -0500

    Re-enable the all connections test

--- spec/0_identity_spec.rb
+++ spec/0_identity_spec.rb
@@ -19,21 +19,21 @@ describe 'Getting Started', :markdown =>
     expect(Pacto).to_not have_unmatched_requests
   end
 
-  # validate_challenge 'all connections', """
-  # Let's make a connection to each of the available OpenStack products
-  # """, standard_env_vars, [] do
-  #   expect(Pacto).to have_validated_service('Identity', 'Authenticate')
-  #   expect(Pacto).to have_validated(:get, Addressable::Template.new('https://ord.servers.api.rackspacecloud.com/v2/{token_id}/servers/detail'))
-  #   expect(Pacto).to have_validated(:get, Addressable::Template.new('https://ord.servers.api.rackspacecloud.com/v2/{token_id}/os-networksv2'))
-  #   expect(Pacto).to have_validated(:get, Addressable::Template.new('https://storage101.ord1.clouddrive.com/v1/{mosso_tenant_id}/{?format}'))
-  #   expect(Pacto).to have_validated(:get, Addressable::Template.new('https://ord.loadbalancers.api.rackspacecloud.com/v1.0/{token_id}/loadbalancers'))
-  #   expect(Pacto).to have_validated(:get, Addressable::Template.new('https://ord.databases.api.rackspacecloud.com/v1.0/{token_id}/instances'))
-  #   expect(Pacto).to have_validated(:get, Addressable::Template.new('https://dns.api.rackspacecloud.com/v1.0/{token_id}/domains'))
-  #   expect(Pacto).to have_validated(:get, Addressable::Template.new('https://monitoring.api.rackspacecloud.com/v1.0/{token_id}/account'))
-  #   expect(Pacto).to have_validated(:get, Addressable::Template.new('https://ord.blockstorage.api.rackspacecloud.com/v1/{token_id}/volumes'))
-  #   expect(Pacto).to have_validated(:get, Addressable::Template.new('https://ord.autoscale.api.rackspacecloud.com/v1.0/{token_id}/groups'))
-  #   expect(Pacto).to have_validated(:get, Addressable::Template.new('https://ord.queues.api.rackspacecloud.com/v1/{token_id}/queues'))
-  #   expect(Pacto).to_not have_failed_validations
-  #   expect(Pacto).to_not have_unmatched_requests
-  # end
+  validate_challenge 'all connections', """
+  Let's make a connection to each of the available OpenStack products
+  """, standard_env_vars, [] do
+    expect(Pacto).to have_validated_service('Identity', 'Authenticate')
+    expect(Pacto).to have_validated_service('Cloud Servers', 'Get Server Details')
+    expect(Pacto).to have_validated_service('Cloud Networks', 'List Networks')
+    expect(Pacto).to have_validated_service('Cloud Files', 'List Containers')
+    expect(Pacto).to have_validated_service('Cloud Load Balancers', 'List Load Balancers')
+    expect(Pacto).to have_validated_service('Cloud Databases', 'List Instances')
+    expect(Pacto).to have_validated_service('DNS', 'List Domains')
+    expect(Pacto).to have_validated_service('Cloud Monitoring', 'Get Account')
+    expect(Pacto).to have_validated_service('Cloud Block Storage', 'List Volumes')
+    expect(Pacto).to have_validated_service('Autoscale', 'List Groups')
+    expect(Pacto).to have_validated_service('Cloud Queues', 'List Queues')
+    expect(Pacto).to_not have_failed_validations
+    expect(Pacto).to_not have_unmatched_requests
+  end
 end
