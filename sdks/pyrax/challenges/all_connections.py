ab3fcd59c8a0c13f5324c1102d4bf8a42128ec05
Max Lincoln <max@devopsy.com>
2014-03-13 12:59:12 -0400

    Pyrax workaround for 'No identity class has been defined for the current environment'

--- sdks/pyrax/challenges/all_connections.py
+++ sdks/pyrax/challenges/all_connections.py
@@ -3,16 +3,15 @@
 import os
 import pyrax
 
-pyrax.set_http_debug(True)
-pyrax.set_setting("region", os.getenv("RAX_REGION"))
 pyrax.set_setting("identity_type", "rackspace")
 # Create the identity object
 pyrax._create_identity()
 # Change its endpoint
 pyrax.identity.auth_endpoint = os.getenv('RAX_AUTH_URL') + '/v2.0/'
 
-# Identity Connection - Authenticate
+# Authenticate
 pyrax.set_credentials(os.getenv('RAX_USERNAME'), os.getenv('RAX_API_KEY'))
+
 print "Authenticated"
 
 # Cloud Servers API - List Servers
--- sdks/pyrax/challenges/get_object_metadata.py
+++ sdks/pyrax/challenges/get_object_metadata.py
@@ -3,16 +3,15 @@
 import os
 import pyrax
 
-pyrax.set_http_debug(True)
-pyrax.set_setting("region", os.getenv("RAX_REGION"))
 pyrax.set_setting("identity_type", "rackspace")
 # Create the identity object
 pyrax._create_identity()
 # Change its endpoint
 pyrax.identity.auth_endpoint = os.getenv('RAX_AUTH_URL') + '/v2.0/'
 
-# Identity Connection - Authenticate
+# Authenticate
 pyrax.set_credentials(os.getenv('RAX_USERNAME'), os.getenv('RAX_API_KEY'))
+
 # Cloud Files API - List Files
 cf = pyrax.cloudfiles
 cf.get_object_metadata(os.getenv('TEST_DIRECTORY'), os.getenv('TEST_FILE'))
