87618afd0c2811db8da25bd4fdeba4385e83ee85
Max Lincoln <max@devopsy.com>
2014-05-07 15:55:18 -0400

    Ignore files based on .gitignore

--- lib/polytrix/core/file_finder.rb
+++ lib/polytrix/core/file_finder.rb
@@ -3,18 +3,15 @@ module Polytrix
     module FileFinder
       class FileNotFound < StandardError; end
 
-      # Incomplete list
-      SOURCE_FILE_EXTENSIONS = %(.java .rb .js .go .php .cs .py)
-      DOC_FILE_EXTENSIONS = %(.md .asciidoc .rdoc .erb)
-
       # Finds a file by loosely matching the file name to a scenario name
       def find_file(search_path, scenario_name)
+        ignored_patterns = read_gitignore(search_path)
         potential_files = Dir.glob("#{search_path}/**/*#{scenario_name}.*", File::FNM_CASEFOLD)
         potential_files.concat Dir.glob("#{search_path}/**/*#{scenario_name.gsub('_', '')}.*", File::FNM_CASEFOLD)
 
         # Find the first file, not including generated files
         file = potential_files.find { |file|
-          is_source?(file) || is_doc?(file)
+          !ignored? ignored_patterns, search_path, file
         }
 
         fail FileNotFound, "No file was found for #{scenario_name} within #{search_path}" if file.nil?
@@ -23,12 +20,23 @@ module Polytrix
 
       private
 
-      def is_source?(file)
-        SOURCE_FILE_EXTENSIONS.include? File.extname file
+      def read_gitignore(dir)
+        gitignore_file = "#{dir}/.gitignore"
+        File.read(gitignore_file)
+      rescue
+        ""
       end
 
-      def is_doc?(file)
-        DOC_FILE_EXTENSIONS.include? File.extname(file)
+      def ignored?(ignored_patterns, base_path, target_file)
+        ignored_patterns.split.find do |pattern|
+          # if git ignores a folder, we should ignore all files it contains
+          pattern = "#{pattern}**" if pattern[-1] == '/'
+          relativize(target_file, base_path).fnmatch? pattern
+        end
+      end
+
+      def relativize(file, base_path)
+        Pathname.new(file).relative_path_from Pathname.new(base_path)
       end
     end
   end
