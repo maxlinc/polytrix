9b511b33f0048a1ab21293fa49abb2cabc929199
Max Lincoln <max@devopsy.com>
2014-05-13 18:04:50 -0400

    Some YARD

--- lib/polytrix.rb
+++ lib/polytrix.rb
@@ -14,49 +14,64 @@ require 'polytrix/rspec'
 
 module Polytrix
   class << self
+    # The set of {Polytrix::Implementor}s registered with Polytrix.
     attr_accessor :implementors
+    # The {Polytrix::Manifest} that describes the test scenarios known to Polytrix.
     attr_accessor :manifest
-    attr_accessor :default_validator_callback
 
+    # Invokes the bootstrap  action for each SDK.
+    # @see Polytrix::Implementor#bootstrap
     def bootstrap
       implementors.each do |implementor|
         implementor.bootstrap
       end
     end
 
+    # Loads the {manifest} from a YAML file.
+    def load_manifest(yaml_file)
+      @manifest = Polytrix::Manifest.from_yaml yaml_file
+    end
+
+    # Runs all of the tests described in the {manifest}
+    def run_tests
+      Polytrix::RSpec.run_manifest(@manifest)
+    end
+
+    # The callback used to validate code samples that
+    # don't have a custom validator.  The default 
+    # checks that the sample code runs successfully.
     def default_validator_callback
       @default_validator_callback ||= proc{ |challenge|
         expect(challenge[:result].execution_result.exitstatus).to eq(0)
       }
     end
 
+    # Sets a new default validator to use with code
+    # samples that don't have a custom validator.
     def default_validator_callback=(callback)
       @default_validator_callback = callback
     end
 
+    # @see Polytrix::Configuration
     def configuration
       fail "configuration doesn't take a block, use configure" if block_given?
       @configuration ||= Configuration.new
     end
 
+    # @see Polytrix::Configuration
     def configure
       yield(configuration)
     end
 
+    # Merges multiple test results files produced the rspec {Polytrix::RSpec::YAMLReport} formatter.
+    # @param result_files [Array] the location of the files to merge.
+    # @return [String] the merged content
     def merge_results(result_files)
       merged_results = Polytrix::Manifest.new
       result_files.each do |result_file|
         merged_results.deep_merge! YAML::load(File.read(result_file))
       end
       YAML::dump(merged_results.to_hash)
-    end
-
-    def load_manifest(yaml_file)
-      @manifest = Polytrix::Manifest.from_yaml yaml_file
-    end
-
-    def run_tests
-      Polytrix::RSpec.run_manifest(@manifest)
     end
   end
 end
