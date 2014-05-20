f8b95e26d4bb34f7acdcdbbbda155af28102047f
Max Lincoln <max@devopsy.com>
2014-05-20 18:05:55 -0400

    YARDing

--- lib/polytrix/manifest.rb
+++ lib/polytrix/manifest.rb
@@ -5,6 +5,33 @@ require 'hashie/extensions/coercion'
 require 'hashie/extensions/deep_merge'
 
 module Polytrix
+  # Polytrix::Manifest acts as a test manifest. It defines the test scenarios that should be run,
+  # and may be shared across multiple implementors when used for a compliance suite.
+  # 
+  # A manifest is generally defined and loaded from YAML. Here's an example manifest:
+  #   ---
+  #   global_env:
+  #     LOCALE: <%= ENV['LANG'] %>
+  #     FAVORITE_NUMBER: 5
+  #   suites:
+  #     Katas:
+  #       env:
+  #         NAME: 'Max'
+  #       samples:
+  #         - hello world
+  #         - quine
+  #     Tutorials:
+  #           env:
+  #           samples:
+  #             - deploying
+  #             - documenting
+  #
+  # The *suites* object defines the tests. Each object, under suites, like *Katas* or *Tutorials* in this
+  # example, represents a test suite. A test suite is subdivided into *samples*, that each act as a scenario.
+  # The *global_env* object and the *env* under each suite define (and standardize) the input for each test.
+  # The *global_env* values will be made available to all tests as environment variables, along with the *env*
+  # values for that specific test.
+  # 
   class Manifest < Hashie::Dash
     include Hashie::Extensions::DeepMerge
 
@@ -42,6 +69,7 @@ module Polytrix
     property :suites
     coerce_key :suites, Polytrix::Manifest::Suites
 
+    # Parses a YAML file to create a {Manifest} object.
     def self.from_yaml(yaml_file)
       raw_content = File.read(yaml_file)
       processed_content = ERB.new(raw_content).result
