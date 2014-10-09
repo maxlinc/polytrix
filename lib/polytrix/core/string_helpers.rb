b9e845c2c66cb5ec4aeeecdc28cc6cc5591c85ca
Max Lincoln <max@devopsy.com>
2014-10-09 16:03:53 -0400

    Don't need Ruby 1.8 workarounds

--- lib/polytrix/core/string_helpers.rb
+++ lib/polytrix/core/string_helpers.rb
@@ -92,13 +92,7 @@ module Polytrix
       '"' => '&quot;',
       '/' => '&#x2F;'
     }
-    if //.respond_to?(:encoding)
-      ESCAPE_HTML_PATTERN = Regexp.union(*ESCAPE_HTML.keys)
-    else
-      # On 1.8, there is a kcode = 'u' bug that allows for XSS otherwise
-      # TODO: doesn't apply to jruby, so a better condition above might be preferable?
-      ESCAPE_HTML_PATTERN = /#{Regexp.union(*ESCAPE_HTML.keys)}/n
-    end
+    ESCAPE_HTML_PATTERN = Regexp.union(*ESCAPE_HTML.keys)
 
     # Escape ampersands, brackets and quotes to their HTML/XML entities.
     def self.escape_html(string)
