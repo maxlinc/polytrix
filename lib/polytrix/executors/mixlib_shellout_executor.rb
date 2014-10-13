e27eea072edab11d637cd7727d4811661a35986e
Max Lincoln <max@devopsy.com>
2014-10-12 21:38:47 -0400

    Remove dead class

--- lib/polytrix/executors/mixlib_shellout_executor.rb
+++ lib/polytrix/executors/mixlib_shellout_executor.rb
@@ -33,8 +33,6 @@ module Polytrix
       end.compact
 
       def log_decorator(_io, _prefix)
-        # OutputDecorator.new(io, prefix) unless Polytrix.configuration.suppress_output
-        # logger = Logging.logger['polytrix::exec']
         IOToLog.new(logger)
       end
 
@@ -51,39 +49,6 @@ module Polytrix
         execution_error = ExecutionError.new(e)
         execution_error.execution_result = execution_result
         raise execution_error
-      end
-
-      class OutputDecorator
-        # Reserve :red, :black, :white
-        COLORS = [:green, :yellow, :blue, :magenta, :cyan]
-
-        def self.next_color
-          @next_color ||= 0
-          @next_color += 1
-          COLORS[@next_color % COLORS.size]
-        end
-
-        def initialize(real_io, prefix = nil)
-          @real_io = real_io
-          # @prefix = set_color(prefix, :cyan)
-          @prefix = "#{prefix}: " if prefix
-          @color = self.class.next_color
-          @thor_shell = Thor::Shell::Color.new
-        end
-
-        def puts(line)
-          line = line.gsub(/^/, @prefix) if @prefix
-          @real_io.puts @thor_shell.set_color(line, @color)
-        end
-
-        def <<(line)
-          line = line.gsub(/^/, @prefix) if @prefix
-          @real_io << @thor_shell.set_color(line, @color)
-        end
-
-        def method_missing(meth, *args, &block)
-          @real_io.send meth, *args, &block
-        end
       end
     end
   end
