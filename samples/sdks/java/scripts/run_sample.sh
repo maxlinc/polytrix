a8917b7955d0d040608139c5b96c820db7c362b8
Max Lincoln <max@devopsy.com>
2015-01-08 18:34:39 -0500

    Move samples to separate projects (fetch via polytrix clone)

--- samples/sdks/java/scripts/run_sample.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-SOURCE_FILE=$1
-CLASSNAME=`basename $SOURCE_FILE .java`
-#NAMESPACE=org.example.
-CLASS="${NAMESPACE}${CLASSNAME}"
-gradle assemble --quiet
-java -classpath build/libs/java-1.0.jar $CLASS
-
--- samples/sdks/ruby/challenges/hello_world.rb
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env ruby
-name = 'world'
-# This will print Hello, world!
-puts "Hello, #{name}!"
